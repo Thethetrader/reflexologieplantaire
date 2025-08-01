22499df370e8be9b22038725faa0bf4d972e1980
Theodore Brey <theodorebrey@MacBook-Pro-de-Theodore-2.local>
2025-08-01 12:36:22 +0200

    Move hero foot image up on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             src="/logo-reflexologie-2025-lighter.jpg"
             alt="Reflexologie background"
             fill
-            className="object-contain h-40 md:object-contain md:h-auto mt-48 md:mt-0"
+            className="object-contain h-40 md:object-contain md:h-auto mt-32 md:mt-0"
             style={{ objectPosition: 'center' }}
           priority
         />
